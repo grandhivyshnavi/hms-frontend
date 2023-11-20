8d2ae6d262c104a359830aa5812aa0e4c3ff66d3
Grandhi Vyshnavi <110164137+grandhivyshnavi@users.noreply.github.com>
2023-11-20 17:41:07 +0530

    CreateDoctorAdmin.js

--- src/components/CreateDoctorAdmin.js
+++ src/components/CreateDoctorAdmin.js
@@ -1,87 +1,87 @@
-// import { useState } from "react";
-// import DoctorAdminForm from "./DoctorAdminForm";
-// import Axios from "axios";
-// import Nav3 from './Nav3';
-
-// function CreateDoctorAdmin() {
-//     const [arr, setArr] = useState([]);
-//     const getState = (childData) => {
-//         setArr(childData);
-//     }
-
-//     const handleSubmit = () => {
-//         const data = { name: arr[0], email: arr[1], contact: arr[2], specialization: arr[3] }
-//         Axios.post("https://hms-deployment-backend.onrender.com/doctoradminRoute/create-doctoradmin", data)
-//             .then((res) => {
-//                 if (res.status === 200)
-//                     alert("Record added successfully")
-//                 else
-//                     Promise.reject();
-//             })
-//             .catch((err) => alert(err));
-//     }
-//     return (
-//         <div><Nav3/>
-//             <form onSubmit={handleSubmit}>
-//                 <DoctorAdminForm getState={getState} nameValue="" emailValue="" contactValue="" specializationValue="">Add Doctor</DoctorAdminForm>
-//             </form>
-//         </div>
-//     )
-// }
-// export default CreateDoctorAdmin;
-
-
-
 import { useState } from "react";
 import DoctorAdminForm from "./DoctorAdminForm";
 import Axios from "axios";
-import Nav3 from "./Nav3";
+import Nav3 from './Nav3';
 
 function CreateDoctorAdmin() {
     const [arr, setArr] = useState([]);
-
     const getState = (childData) => {
         setArr(childData);
-    };
+    }
 
-    const handleSubmit = (event) => {
-        event.preventDefault(); // Prevent the default form submission behavior
-
-        const data = {
-            name: arr[0],
-            email: arr[1],
-            contact: arr[2],
-            specialization: arr[3],
-        };
-
+    const handleSubmit = () => {
+        const data = { name: arr[0], email: arr[1], contact: arr[2], specialization: arr[3] }
         Axios.post("https://hms-deployment-backend.onrender.com/doctoradminRoute/create-doctoradmin", data)
             .then((res) => {
-                if (res.status === 200) {
-                    alert("Record added successfully");
-                    setArr([]); // Clear the form fields after successful submission
-                } else {
+                if (res.status === 200)
+                    alert("Record added successfully")
+                else
                     Promise.reject();
-                }
             })
             .catch((err) => alert(err));
-    };
-
+    }
     return (
-        <div>
-            <Nav3 />
+        <div><Nav3/>
             <form onSubmit={handleSubmit}>
-                <DoctorAdminForm
-                    getState={getState}
-                    nameValue=""
-                    emailValue=""
-                    contactValue=""
-                    specializationValue=""
-                >
-                    Add Doctor
-                </DoctorAdminForm>
+                <DoctorAdminForm getState={getState} nameValue="" emailValue="" contactValue="" specializationValue="">Add Doctor</DoctorAdminForm>
             </form>
         </div>
-    );
+    )
 }
-
 export default CreateDoctorAdmin;
+
+
+
+// import { useState } from "react";
+// import DoctorAdminForm from "./DoctorAdminForm";
+// import Axios from "axios";
+// import Nav3 from "./Nav3";
+
+// function CreateDoctorAdmin() {
+//     const [arr, setArr] = useState([]);
+
+//     const getState = (childData) => {
+//         setArr(childData);
+//     };
+
+//     const handleSubmit = (event) => {
+//         event.preventDefault(); // Prevent the default form submission behavior
+
+//         const data = {
+//             name: arr[0],
+//             email: arr[1],
+//             contact: arr[2],
+//             specialization: arr[3],
+//         };
+
+//         Axios.post("https://hms-deployment-backend.onrender.com/doctoradminRoute/create-doctoradmin", data)
+//             .then((res) => {
+//                 if (res.status === 200) {
+//                     alert("Record added successfully");
+//                     setArr([]); // Clear the form fields after successful submission
+//                 } else {
+//                     Promise.reject();
+//                 }
+//             })
+//             .catch((err) => alert(err));
+//     };
+
+//     return (
+//         <div>
+//             <Nav3 />
+//             <form onSubmit={handleSubmit}>
+//                 <DoctorAdminForm
+//                     getState={getState}
+//                     nameValue=""
+//                     emailValue=""
+//                     contactValue=""
+//                     specializationValue=""
+//                 >
+//                     Add Doctor
+//                 </DoctorAdminForm>
+//             </form>
+//         </div>
+//     );
+// }
+
+// export default CreateDoctorAdmin;
